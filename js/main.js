8617bdfb2bf7efaab9f86630ce7db223d9b2bea0
Kristl Yuen <kristlyuen@users.noreply.github.com>
2022-07-12 19:18:31 -0400

    Updated files to randomize the birdcall that is returned and add a message if no birdcall could be found.

--- js/main.js
+++ js/main.js
@@ -2,22 +2,32 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('select').value
-  const url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=cnt:"${choice}"+q:A`
-  // console.log(choice);
+  const url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q_gt:C+len:10+cnt:"${choice}"`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(Math.floor(Math.random() * 499))
-        console.log(data.recordings[Math.floor(Math.random() * 499)])
-        document.querySelector('h2').innerHTML = data.recordings[Math.floor(Math.random() * 499)].en
-        document.querySelector('audio').src = data.recordings[Math.floor(Math.random() * 499)].file
+        const random = Math.floor(Math.random() * Number(data.numRecordings))
+        console.log(random)
+        // If there are no recordings, tell the user that they need to pick another country.
+        // How can we remove recordings with 'Identity unknown' from the options altogether? Loop through and pop them off?
+        if (random === 0 || data.recordings[random].en === 'Identity unknown') {
+          document.querySelector('h2').innerHTML = 'Sorry, there are no birdsongs for that country. Please pick another country.'
+          document.querySelector('audio').classList.add('hidden')
+          document.querySelector('p').classList.add('hidden')
+        }
+        else {
+          document.querySelector('audio').classList.remove('hidden')
+          document.querySelector('p').classList.remove('hidden')
+          document.querySelector('audio').src = data.recordings[random].file
+          document.querySelector('h2').innerHTML = data.recordings[random].en
+          document.querySelector('p').innerHTML = `This birdsong was recorded on ${data.recordings[random].date} at ${data.recordings[random].loc}.`
+        }  
+        console.log(data)
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
-// Seems like there are max 499 birds on a page = Math.floor(Math.random() * 499)
-// If there are less than 499 birds total = Math.floor(Math.random() * data.numRecordings)
-// data.recordings[data.numRecordings]
+
