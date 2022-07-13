50ee828c6a53a70d05e68233ac3e993d85be643f
Kristl Yuen <kristlyuen@users.noreply.github.com>
2022-07-13 18:45:28 -0400

    Updated randomization and filtered out results with unknown identity.

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('select').value
+  // const url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q_gt:C+len:10+cnt:"${choice}"`
   const url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q_gt:C+len:10+cnt:"${choice}"`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        const random = Math.floor(Math.random() * Number(data.numRecordings))
+        const random = Math.ceil(Math.random() * Number(data.numRecordings))
         console.log(random)
+        const unknownIdentityFilteredOut = data.recordings.filter(x => x.gen !== 'Mystery')
         // If there are no recordings, tell the user that they need to pick another country.
-        // How can we remove recordings with 'Identity unknown' from the options altogether? Loop through and pop them off?
-        if (random === 0 || data.recordings[random].en === 'Identity unknown') {
+        if (random === 0) {
           document.querySelector('h2').innerHTML = `Sorry, we couldn't find a birdcall. Please try again.`
           document.querySelector('audio').classList.add('hidden')
           document.querySelector('p').classList.add('hidden')
@@ -19,9 +20,9 @@ function getFetch(){
         else {
           document.querySelector('audio').classList.remove('hidden')
           document.querySelector('p').classList.remove('hidden')
-          document.querySelector('audio').src = data.recordings[random].file
-          document.querySelector('h2').innerHTML = data.recordings[random].en
-          document.querySelector('p').innerHTML = `This birdsong was recorded on ${data.recordings[random].date} at ${data.recordings[random].loc}.`
+          document.querySelector('audio').src = unknownIdentityFilteredOut[random - 1].file
+          document.querySelector('h2').innerHTML = unknownIdentityFilteredOut[random - 1].en
+          document.querySelector('p').innerHTML = `This birdsong was recorded on ${unknownIdentityFilteredOut[random - 1].date} at ${unknownIdentityFilteredOut[random - 1].loc}.`
         }  
         console.log(data)
       })
