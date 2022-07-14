1b7119fbcf6910ffac0ac19414abc4f2f232fe82
Kristl Yuen <kristlyuen@users.noreply.github.com>
2022-07-14 18:38:32 -0400

    add antarctica urlconditional, remove unnecessary country conditional, add comments

--- js/main.js
+++ js/main.js
@@ -2,28 +2,37 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('select').value
-  // const url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q_gt:C+len:10+cnt:"${choice}"`
-  const url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q:A+len:10+area:${choice}`
+  let url 
+  
+  // If the user selects Antarctica, use this URL with the country query parameter for Antarctica. This r
+  if (choice === 'antarctica') {
+    url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q_gt:C+len:10+cnt:"${choice}"`
+  }
+  // Otherwise, use this URL with the area query parameter.
+  else {
+    url = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q:A+len:12+area:${choice}`
+  }  
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
+        // Declare a random number based on the total number of recordings returned.
         const random = Math.ceil(Math.random() * Number(data.numRecordings))
         console.log(random)
+        // Filter out any birdcalls without known identities.
         const unknownIdentityFilteredOut = data.recordings.filter(x => x.gen !== 'Mystery')
-        // If there are no recordings, tell the user that they need to pick another country.
-        if (random === 0) {
-          document.querySelector('h2').innerHTML = `Sorry, we couldn't find a birdcall. Please try again.`
-          document.querySelector('audio').classList.add('hidden')
-          document.querySelector('p').classList.add('hidden')
-        }
-        else {
+
+          // Only show the audio player and text when the "Get birdsong" button is clicked.
           document.querySelector('audio').classList.remove('hidden')
           document.querySelector('p').classList.remove('hidden')
+
+          // Return the audio file and the common name of the bird.
           document.querySelector('audio').src = unknownIdentityFilteredOut[random - 1].file
           document.querySelector('h2').innerHTML = unknownIdentityFilteredOut[random - 1].en
-          document.querySelector('p').innerHTML = `This birdsong was recorded on ${unknownIdentityFilteredOut[random - 1].date} at ${unknownIdentityFilteredOut[random - 1].loc}.`
-        }  
+
+          // Return the date, location, and country where the birdcall was recorded.
+          document.querySelector('p').innerHTML = `This birdsong was recorded on ${unknownIdentityFilteredOut[random - 1].date} at ${unknownIdentityFilteredOut[random - 1].loc} in ${unknownIdentityFilteredOut[random - 1].cnt}.`
+     
         console.log(data)
       })
       .catch(err => {
