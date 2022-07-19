d9a35b732956ac6bfaa791ab071dac0414ffd067
Kristl Yuen <kristlyuen@users.noreply.github.com>
2022-07-19 11:19:18 -0400

    updated js file name

--- js/main.js
+++ js/main.js
@@ -1,67 +1,90 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+// Follow this guide: https://levelup.gitconnected.com/combining-api-calls-with-javascript-try-catch-ba1b7b9303a5
 
-  // Set variable with the value selected from the dropdown by the user.
-  const choice = document.querySelector('select').value
-
-  // Declare variable for URL to be used based on the value selection.
-  let birdURL 
-  
-  // If the user selects Antarctica, use this URL with the country query parameter for Antarctica. This is because Antarctica is not included in the area query.
-  if (choice === 'antarctica') {
-    birdURL = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q_gt:C+len:10+cnt:"${choice}"`
-  }
-  // Otherwise, use this URL with the area query parameter.
-  else {
-    birdURL = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q:A+len:12+area:${choice}`
-  }  
-
-  fetch(birdURL)
-      // First call the birdcall API.
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        // Declare a random number based on the total number of recordings returned.
-        const random = Math.ceil(Math.random() * Number(data.numRecordings))
-        console.log(random)
-
-        // Filter out any birdcalls without known identities.
-        const unknownIdentityFilteredOut = data.recordings.filter(x => x.gen !== 'Mystery')
-
-        const resultingBirdInfo = unknownIdentityFilteredOut[random - 1]
-
-          // Only show the audio player and text when the "Get birdsong" button is clicked.
-          document.querySelector('audio').classList.remove('hidden')
-          document.querySelector('.recording-info').classList.remove('hidden')
-
-          // Return the audio file and the common name of the bird.
-          document.querySelector('audio').src = resultingBirdInfo.file
-          document.querySelector('h2').innerHTML = resultingBirdInfo.en
-
-          // Return the date, location, and country where the birdcall was recorded.
-          document.querySelector('.recording-info').innerHTML = `This birdsong was recorded on ${resultingBirdInfo.date} at ${resultingBirdInfo.loc} in ${resultingBirdInfo.cnt}.`
-     
-        console.log(resultingBirdInfo)
-        
-      })
-
-      // Then call the Wikipedia API.
-      .then(res => res.json())
-      .then(data => {
-        
-        // Declare a variable for the bird name, transforming all but the first word in the name to lowercase to match parameters for Wikipedia API.
-        const birdName = resultingBirdInfo.en.split(' ').map((el, i) => i == 0 ? el : el.toLowerCase())
-
-        console.log(birdName)
-
-        return fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${birdName}&prop=extracts|pageimages|info&pithumbsize=400&inprop=url&redirects=&format=json&origin=*`)
-
-      })
-
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+function getFetch() {
+const getRandomBird = async () => {
+  const request = await fetch(birdURL);
+  const data = await request.json();
+  return data;
 }
 
+const getWikiInfo = async wikiURL => {
+  const request = await fetch(wikiURL);
+  const data = await request.json();
+  return data;
+}
 
+// Set variable with the value selected from the dropdown by the user.
+const choice = document.querySelector('select').value
 
+// Declare variable for URL to be used based on the value selection.
+let birdURL 
+
+// If the user selects Antarctica, use this URL with the country query parameter for Antarctica. This is because Antarctica is not included in the area query.
+if (choice === 'antarctica') {
+  birdURL = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q_gt:C+len:10+cnt:"${choice}"`
+}
+// Otherwise, use this URL with the area query parameter.
+else {
+  birdURL = `https://gtfo-cors--timmy_i_chen.repl.co/get?url=https://www.xeno-canto.org/api/2/recordings?query=q:A+len:12+area:${choice}`
+}  
+
+getRandomBird().then(bird => {
+  
+  const random = Math.ceil(Math.random() * Number(bird.numRecordings))
+  console.log(random)
+
+  // Filter out any birdcalls without known identities.
+  const unknownIdentityFilteredOut = bird.recordings.filter(x => x.gen!== 'Mystery')
+  const resultingBirdInfo = unknownIdentityFilteredOut[random - 1]
+  
+  // Only show the audio player and text when the "Get birdsong" buttonis clicked.
+  document.querySelector('audio').classList.remove('hidden')
+  document.querySelector('.recording-info').classList.remove('hidden')
+  
+  // Return the audio file and the common name of the bird.
+  document.querySelector('audio').src = resultingBirdInfo.file
+  document.querySelector('h2').innerText = resultingBirdInfo.en
+  
+  // Return the date, location, and country where the birdcall wasrecorded.
+  document.querySelector('.recording-info').innerText = `This birdsong was recorded on ${resultingBirdInfo.date} at ${resultingBirdInfo.loc}in ${resultingBirdInfo.cnt}.`
+  
+  console.log(resultingBirdInfo)
+
+  // Declare a variable for the bird name, transforming all but the first word in the name to lowercase to match parameters for Wikipedia API.
+  const birdName = resultingBirdInfo.en.split(' ').map((el, i) => i == 0 ? el : el.toLowerCase()).join('%20')
+  console.log(birdName)
+
+  getWikiInfo(`https://en.wikipedia.org/w/api.php?action=query&titles=${birdName}&prop=extracts|pageimages|info&pithumbsize=400&inprop=url&redirects=&format=json&origin=*`).then(wikiData => {
+    
+    const processWikiResults = results => {
+      const resultArray = []
+      Object.keys(results).forEach(key => {
+        const id = key;
+        const title = results[key].title
+        const text = results[key].extract
+        const img = results[key].hasOwnProperty('thumbnail')
+          ? results[key].thumbnail.source
+          : null;
+          const item = {
+            id: id,
+            title: title,
+            img: img,
+            text: text
+          }
+        resultArray.push(item)
+      })
+      console.log(resultArray)
+    }
+    
+    
+    console.log(wikiData)
+  }) 
+  
+  
+})
+}
+
+// link to get main image
+// http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=${birdName}&origin=*
